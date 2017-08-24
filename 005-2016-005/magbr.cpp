3f6fe0ac13695c8c2e436ec6570285202f2bd23b
Novak Petrovic <npetrovic@gmail.com>
2017-08-24 19:11:22 +0100

    Dodavanje cpp fajlova

---
+++ 005-2016-005/magbr.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main(void) {
+	int N = 1000;
+	char unesen_broj[N];
+	//
+	cin.getline(unesen_broj, N+1);
+	//
+	string broj = "";
+	//
+	int i = 0;
+	bool magican = true;
+	while (unesen_broj[i] != '\0') {
+		broj = broj + unesen_broj[i];
+		if (atoi(broj.c_str()) % (i+1) != 0) {
+			magican = false;
+		}
+		i++;
+	}
+	//
+	if (magican == true) {
+		cout << "Broj je magican!" << endl;
+	}
+	else {
+		cout << "Broj nije magican!" << endl;
+	}
+}
---
+++ 005-2016-005/matrica.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cstdlib>
+#include <stdio.h>
+
+using namespace std;
+
+int main(void) {
+	int r, k;
+	scanf("%d,%d", &r, &k);
+	int nur[r];
+	int matrica[r][k];
+	for (int i = 0; i < r; i++) {
+		for (int j = 0; j < k; j++) {
+			cin >> matrica[i][j];
+		}
+	}
+	for (int i = 0; i < r; i++) {
+		nur[i] = matrica[i][0];
+		for (int j = 0; j < k; j++) {
+			if (matrica[i][j] < nur[i]) {
+				nur[i] = matrica[i][j];
+			}
+		}
+	}
+	//
+	int najveci = nur[0];
+	for (int i = 1; i < r; i++) {
+		if (nur[i] > najveci) {
+			najveci = nur[i];
+		}
+	}
+	cout << "Trazeni broj je: " << najveci << "." << endl;
+	//
+	for (int i = 0; i < r; i++) {
+		for (int j = 0; j < k; j++) {
+			if (matrica[i][j] == najveci) {
+				cout << "Nalazi se u " << (i+1) << ". redu i " << (j+1) << ". koloni. " << endl;
+			}
+		}
+	}	
+}
