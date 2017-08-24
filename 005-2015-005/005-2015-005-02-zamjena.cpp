24e1f9e9785e988a465837f5fd29a67d7e07bad7
Novak Petrovic <npetrovic@gmail.com>
2017-08-24 19:32:21 +0100

    Два задатка из 2015. године

---
+++ 005-2015-005/005-2015-005-02-zamjena.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main(void) {
+	int N = 1000;
+	char recenica[N];
+	string rijeci[N];
+	string trazena_rijec;
+	string rijec_za_zamjenu;
+	//
+	for (int i = 0; i < N; i++) {
+		recenica[i] = '^';
+	}
+	//
+	for (int i = 0; i < N; i++) {
+		rijeci[i] = "***";
+	}
+	//
+	cin.getline(recenica, N+1);
+	//
+	getline(cin, trazena_rijec);
+	getline(cin, rijec_za_zamjenu);
+	//
+	int j = 0;
+	int k = 0;
+	string trenutna_rijec = "";
+	while (recenica[j] != '^') {
+		if (recenica[j] == ' ' || recenica[j] == '\0') {
+			rijeci[k] = trenutna_rijec;
+			trenutna_rijec = "";
+			k++;
+		}
+		else {
+			trenutna_rijec = trenutna_rijec + recenica[j];
+		}
+		j++;
+	}
+	//
+	for (int i = 0; i < k; i++) {
+		if (rijeci[i] != trazena_rijec) {
+			cout << rijeci[i] << " ";
+		}
+		else {
+			cout << rijec_za_zamjenu << " ";
+		}
+	}
+	cout << endl;
+}
