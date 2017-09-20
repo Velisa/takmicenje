46ddf0e3928b805660bf712b442744fb5a077852
Velisa Petricevic <53cevic@gmail.com>
2017-09-20 23:41:19 +0200

    Ispravljen 005-2017-005-03

--- 005-2017-005/005-2017-005-03/005-2017-005-03.cpp
+++ 005-2017-005/005-2017-005-03/005-2017-005-03.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 
+typedef unsigned int uint;
+
 bool jeMali(int64_t i){
     return i <= 1000000000;
+}
+
+int manjiOd(int i1, int i2){
+    return (i1 > i2 ? i2 : i1);
+}
+
+int veciOd(int i1, int i2){
+    return (i1 > i2 ? i1 : i2);
 }
 
 int main(){
@@ -14,12 +24,61 @@ int main(){
     }
     if((x1 == x2 && x2 == x3) || (y1 == y2 && y2 == y3)){
         std::cout << 1 << std::endl;
-    }else if(
-            (x1 == x2 && (y2 == y3 || y1 == y3)) ||
-            (x2 == x3 && (y1 == y2 || y1 == y3)) ||
-            (x1 == x3 && (y1 == y2 || y2 == y3))){
-        std::cout << 2 << std::endl;
     }else{
+        uint slucaj1 = 0;
+        uint slucaj2 = 0;
+
+        /*
+            1 = 1 i 2
+            2 = 2 i 3
+            3 = 1 i 3
+        */
+
+        if(x1 == x2) slucaj1 = 1;
+        if(x2 == x3) slucaj1 = 2;
+        if(x1 == x3) slucaj1 = 3;
+
+        if(y1 == y2) slucaj2 = 1;
+        if(y2 == y3) slucaj2 = 2;
+        if(y1 == y3) slucaj2 = 3;
+        if(slucaj1 > 0 && slucaj2 > 0){
+            std::cout << 2 << std::endl;
+            return 0;
+        }
+
+        if(slucaj1 == 1 && (y3 >= veciOd(y1,y2) || y3 <= manjiOd(y1,y2))){
+            std::cout << 2 << std::endl;
+            return 0;
+        }
+        
+        if(slucaj1 == 2 && (y1 >= veciOd(y2,y3) || y1 <= manjiOd(y2,y3))){
+            std::cout << 2 << std::endl;
+            return 0;
+        }
+        
+        if(slucaj1 == 3 && (y2 >= veciOd(y1,y3) || y2 <= manjiOd(y1,y3))){
+            std::cout << 2 << std::endl;
+            return 0;
+        }
+
+        //-----------------------------------------------------------------
+
+        if(slucaj2 == 1 && (x3 >= veciOd(x1,x2) || x3 <= manjiOd(x1,x2))){
+            std::cout << 2 << std::endl;
+            return 0;
+        }
+        
+        if(slucaj2 == 2 && (x1 >= veciOd(x2,x3) || x1 <= manjiOd(x2,x3))){
+            std::cout << 2 << std::endl;
+            return 0;
+        }
+        
+        if(slucaj2 == 3 && (x2 >= veciOd(x1,x3) || x2 <= manjiOd(x1,x3))){
+            std::cout << 2 << std::endl;
+            return 0;
+        }
+
+
         std::cout << 3 << std::endl;
     }
 }
