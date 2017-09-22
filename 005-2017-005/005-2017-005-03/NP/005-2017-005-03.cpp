e4c881931628f5ee8da9d402ef1f06d8628095e6
Novak Petrovic <npetrovic@gmail.com>
2017-09-22 15:18:27 +0100

    Рјешење трећег задатка

--- 005-2017-005/005-2017-005-03/NP/005-2017-005-03.cpp
+++ 005-2017-005/005-2017-005-03/NP/005-2017-005-03.cpp
@@ -2,6 +2,7 @@
  * Released under GNU General Public License v3.0 */
 
 // Za zadate test primjere, int tipovi su dovoljni
+//
 #include <iostream>
 using namespace std;
 typedef unsigned int uint;
@@ -29,13 +30,13 @@ int main() {
 		int slucaj2 = 0;
 		if (x1==x2) slucaj1 = 1;
 		if (x2==x3) slucaj1 = 2;
-    if (x1==x3) slucaj1 = 3;
+		if (x1==x3) slucaj1 = 3;
 		if (y1==y2) slucaj2 = 1;
 		if (y2==y3) slucaj2 = 2;
 		if (y1==y3) slucaj2 = 3;
-    if (slucaj1>0 && slucaj2>0) {
+		if (slucaj1>0 && slucaj2>0) {
 			cout << 2 << endl;
-      return 0;
+			return 0;
 		}
 		// Testovi za slucaj1
 		if (slucaj1==1 && (y3>=veciOd(y1,y2) || y3<=manjiOd(y1,y2))) {
@@ -46,7 +47,7 @@ int main() {
 			cout << 2 << endl;
 			return 0;
 		}
-    if (slucaj1==3 && (y2>=veciOd(y1,y3) || y2<=manjiOd(y1,y3))) {
+		if (slucaj1==3 && (y2>=veciOd(y1,y3) || y2<=manjiOd(y1,y3))) {
 			cout << 2 << endl;
 			return 0;
 		}
@@ -55,11 +56,11 @@ int main() {
 			cout << 2 << endl;
 			return 0;
 		}
-    if (slucaj2==2 && (x1>=veciOd(x2,x3) || x1<=manjiOd(x2,x3))) {
+		if (slucaj2==2 && (x1>=veciOd(x2,x3) || x1<=manjiOd(x2,x3))) {
 			cout << 2 << endl;
 			return 0;
-    }
-    if (slucaj2==3 && (x2>=veciOd(x1,x3) || x2<=manjiOd(x1,x3))) {
+		}
+		if (slucaj2==3 && (x2>=veciOd(x1,x3) || x2<=manjiOd(x1,x3))) {
 			cout << 2 << endl;
 			return 0;
 		}
