ea83f05f4d00eb128d4708ae43a9bb21c0892779
Novak Petrovic <npetrovic@gmail.com>
2017-09-22 15:53:56 +0100

    Сва рјешења завршена и тестирана

---
+++ 005-2017-005/005-2017-005-01/NP/005-2017-005-01.cpp
@@ -0,0 +1,18 @@
+/* Copyright (c) Novak Petrovic
+ * Released under GNU General Public License v3.0 */
+
+#include <math.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+	float x1,y1,r1,x2,y2,r2;
+	cin >> x1 >> y1 >> r1 >> x2 >> y2 >> r2;
+	double d=sqrt(pow(x1-x2,2)+pow(y1-y2,2));
+	if (d==r1+r2)
+		cout << "Krugovi se dodiruju" << endl;
+	else if (d<r1+r2)
+		cout << "Krugovi se preklapaju" << endl;
+	else
+		cout << "Krugovi se ne preklapaju" << endl;
+}
---
+++ 005-2017-005/005-2017-005-02/NP/005-2017-005-02.cpp
@@ -0,0 +1,23 @@
+/* Copyright (c) Novak Petrovic
+ * Released under GNU General Public License v3.0 */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	int niz[n];
+	for (int i=0; i<n; ++i)
+		cin >> niz[i];
+	// Kontrolni broj
+	int kb=niz[0];
+	// Ispisujemo sve brojeve MANJE od kontrolnog broja
+	for (int i=1; i<n; ++i)
+		if(niz[i]<=kb) cout << niz[i] << ' ';
+	//Zatim ispisujemo kontrolni broj
+	cout << kb << ' ';
+	// Ispisujemo sve brojeve VECE od kontrolnog broja
+	for (int i=1; i<n; ++i)
+		if (niz[i]>kb) cout << niz[i] << ' ';
+}
