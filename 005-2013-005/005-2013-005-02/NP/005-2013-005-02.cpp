5e7452802a72b891910f976d1ca28a2968e600d5
Novak Petrovic <npetrovic@gmail.com>
2017-09-23 12:57:53 +0100

    Додао рјешење 005-2013-005-02

---
+++ 005-2013-005/005-2013-005-02/NP/005-2013-005-02.cpp
@@ -0,0 +1,39 @@
+/* Copyright (c) Novak Petrovic
+ * GNU General Public License v3.0 */
+
+#include <math.h>
+#include <iomanip>
+#include <iostream>
+using namespace std;
+
+int main () { 
+  int N; // Broj klijenata
+  int K; // Broj kablova
+  int broj_kablova; // Koliko cijelih kablova mozemo napraviti
+	bool nadjeno=false;
+  float duzina; //  ovoj promjenjljivoj je odgovori
+  float kablovi[5000]; // vo je niz duzina kablovai
+  float ukupnaDuzina=0.0;
+	cin >> N >> K;
+  for (int i=0; i<K; ++i) {
+		cin >> kablovi[i];
+		ukupnaDuzina+=kablovi[i];
+  }
+  duzina=ukupnaDuzina/K; // Maksimalna moguca duzina
+	// Ideja je da se krene od maksimalne moguce duzine 
+	// pojedinacnog kabla i taj broj smanjuje sve dok se
+	// ne dobije odgovarajuci broj potrebnih kablova
+  while (!nadjeno) {
+		broj_kablova=0;
+    for (int i=0; i<K; ++i) {
+      broj_kablova+=(int) floor(kablovi[i]/duzina);
+    }
+    if (broj_kablova == N)
+      nadjeno=true;
+    else
+      /* Veca preciznost je potrebna za neke primjere */
+      duzina -= 0.001;
+  }
+  cout << fixed << setprecision(2) << duzina << endl;
+  return 0;
+}
