4957e9e8809e6e9f71d100667dc52eb78e996341
Novak Petrovic <npetrovic@gmail.com>
2017-09-26 14:16:00 +0100

    Рјешење 005-2013-005-03

--- 005-2013-005/005-2013-005-04/NP/005-2013-005-04.cpp
+++ 005-2013-005/005-2013-005-04/NP/005-2013-005-04.cpp
@@ -1,19 +1,19 @@
-/* Copyright (c) Novak Petrovic GNU General Public License v3.0 */
+/* Copyright (c) Novak Petrovic
+ * GNU General Public License v3.0 */
 
 #include <iostream>
-
 using namespace std;
 
 int main (void) { 
-	int N, M, k, l;
-	int suma = 0;
+	int N,M,k,l;
+	int suma=0;
 	//cout << "Unesite broj redova, N (N <= 5): ";
 	cin >> N;
 	//cout << "Unesite broj kolona, M (M <= 5): ";
 	cin >> M;
 	int matrica[N][M];
-	for (int n = 0; n < N; n++) {
-		for (int m = 0; m < M; m++) {
+	for (int n=0; n<N; ++n) {
+		for (int m=0; m<M; ++m) {
 			//cout << "Unesite " << (n+1)*(m+1) << ". element matrice: ";
 			cin >> matrica[n][m];
 		}
@@ -22,13 +22,13 @@ int main (void) {
 	cin >> k;
 	//cout << "Unesite broj kolone, l (l <= 5): ";
 	cin >> l;
-	for (int n = 0; n < N; n++) {
-		for (int m = 0; m < M; m++) {
+	for (int n=0; n<N; ++n) {
+		for (int m=0; m<M; ++m) {
 			// Ne sabiramo izabrani element
 			if (n != (k-1) &&  m != (l-1)) {
 				// Ako je element na glavnoj dijagonali
 				if (k == l) {
-					if (n==m) {
+					if (n == m) {
 						suma += matrica[n][m];
 					}
 				}
@@ -45,6 +45,7 @@ int main (void) {
 			}
 		}
 	}
-	cout << "Zbir dijagonala je: " << suma << endl;
+	//cout << "Zbir dijagonala je: " << suma << endl;
+	cout << suma << endl;
   return 0;
 }
