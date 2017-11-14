38b1ad9cac391dffd66da81c92b73a86bb6cae14
Novak Petrovic <npetrovic@gmail.com>
2017-11-14 15:53:41 +0000

    Ријешен задатак 005-2015-005-03

---
+++ 005-2015-005/005-2015-005-03/005-2015-005-03.cpp
@@ -0,0 +1,48 @@
+/* Copyright (c) Novak Petrovic
+ * Released under GNU General Public License v3.0 */
+
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+int main() {
+	int N, M, najmanji;
+	int brojRedova = 0;
+  cin >> N;
+	cin >> M;
+	int A[N][M];
+	bool ignorisati[N];
+	// Inicijalizacija
+	for (int i=0; i < N; ++i)
+		ignorisati[i] = false;
+	// Unos matrice
+	for (int i=0; i < N; ++i) 
+  	for (int j=0; j < M; ++j) 
+    	cin >> A[i][j];
+	// Pronaci najmanji broj u cijeloj matrici
+  najmanji = A[0][0];
+  for (int i=0; i < N; ++i)
+  	for (int j = 0; j < M; ++j)
+    	if (A[i][j] < najmanji)
+				najmanji = A[i][j];
+	// Koje redove ignorisati
+  for (int i=0; i < N; ++i)
+  	for (int j=0; j < M; ++j)
+    	if (A[i][j] == najmanji) {
+      	ignorisati[i] = true;
+        ++brojRedova;
+        break;
+			}
+	// Ispis
+	// Napomena: Pazi na zagrade kod vanjskog if-else!
+  if (brojRedova != N) {
+	 	for (int i=0; i < N; ++i)
+			if (ignorisati[i] == false)
+      	for (int j=0; j < M; ++j)
+        	cout << A[i][j] << endl;
+	}
+	else {
+		cout << "Ne postoji matrica koja zadovoljava postavljeni uslov!" << endl;
+	}
+	return EXIT_SUCCESS;
+}
---
+++ 005-2015-005/005-2015-005-03/005-2015-005-03.tests.report.txt
@@ -0,0 +1,10 @@
+Testing 01
+........................TEST 01: PASS
+Testing 02
+........................TEST 02: PASS
+Testing 03
+........................TEST 03: PASS
+Testing 04
+........................TEST 04: PASS
+Testing 05
+........................TEST 05: PASS
