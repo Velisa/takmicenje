9b93b97d703a923cf1ff95f3b3a074afad14f8b7
Novak Petrovic <npetrovic@gmail.com>
2017-11-15 16:07:52 +0000

    Задатак 005-2015-005-02 ријешен

---
+++ 005-2015-005/005-2015-005-02/005-2015-005-02.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	string recenica, trazena, zamjena, prije, poslije;
+	string novaRecenica = "";
+	getline(cin, recenica);
+	cin >> trazena;
+	cin >> zamjena;
+	int pos = recenica.find(trazena);
+	if (pos > -1) {
+		prije = recenica.substr(0, pos);
+		poslije = recenica.substr(pos+trazena.length(), recenica.length());
+		novaRecenica = prije + zamjena + poslije;
+		cout << novaRecenica;
+	}
+	else {
+		cout << "U recenici ne postoji rijec koju treba mijenjati, tako da ";
+    cout << "recenica glasi" << endl;
+		cout << recenica;
+	}
+	return 0;
+}
