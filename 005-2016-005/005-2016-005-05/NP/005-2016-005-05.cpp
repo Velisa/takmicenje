c67a5a9be0fbbc4b077a85e8edc4c0cff7ddfde2
Novak Petrovic <npetrovic@gmail.com>
2017-11-19 14:10:12 +0000

    Solved 005-2016-005-05

---
+++ 005-2016-005/005-2016-005-05/NP/005-2016-005-05.cpp
@@ -0,0 +1,45 @@
+/* Copyright (c) Novak Petrovic
+ * Released under GNU General Public License v3.0 */
+
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  int N, A, B;
+  cin >> N;
+  int niz[N];
+  cin >> A;
+  cin >> B;
+  for (int i=0; i < N; ++i) {
+    cin >> niz[i];
+  }
+  int najveci;
+  int najmanji;
+  bool nadjen = false;
+  for (int i=0; i < N; ++i) {
+    if (niz[i] > A && niz[i] < B ) {
+      najmanji = niz[i];
+      najveci = niz[i];
+      nadjen = true;
+    }
+  }
+  if (nadjen) {
+    for (int i=1; i < N; ++i) {
+      if (niz[i] > A && niz[i] < B && niz[i] < najmanji) {
+        najmanji = niz[i];
+      }
+      if (niz[i] > A && niz[i] < B && niz[i] > najveci) {
+        najveci = niz[i];
+      }
+    }
+    cout << "Najmanji element: " << najmanji << endl;
+    cout << "Najveci element: " << najveci;
+  }
+  else {
+    cout << "Najmanji element: Ne postoji!" << endl;
+    cout << "Najveci element: Ne postoji!";
+  }
+	return 0;
+}
+
+
