a9e0f7b1ddca5fd9aa5491915172f660f4ad6254
Novak Petrovic <npetrovic@gmail.com>
2017-11-19 13:38:05 +0000

    Solved 005-2016-005-02

--- 005-2016-005/005-2016-005-02/NP/005-2016-005-02.cpp
+++ 005-2016-005/005-2016-005-02/NP/005-2016-005-02.cpp
@@ -1,42 +1,39 @@
-/* Copyright (c) Novak Petrovic GNU General Public License v3.0 */
+/* Copyright (c) Novak Petrovic
+ * Released under GNU General Public License v3.0 */
 
 #include <iostream>
-#include <cstdlib>
-#include <stdio.h>
-
 using namespace std;
 
 int main(void) {
 	int r, k;
-	scanf("%d,%d", &r, &k);
+  cin >> r;
+  cin >> k;
 	int nur[r];
 	int matrica[r][k];
-	for (int i = 0; i < r; i++) {
-		for (int j = 0; j < k; j++) {
+	for (int i=0; i < r; ++i) {
+		for (int j=0; j < k; ++j) {
 			cin >> matrica[i][j];
 		}
 	}
-	for (int i = 0; i < r; i++) {
+	for (int i=0; i < r; ++i) {
 		nur[i] = matrica[i][0];
-		for (int j = 0; j < k; j++) {
+		for (int j=0; j < k; ++j) {
 			if (matrica[i][j] < nur[i]) {
 				nur[i] = matrica[i][j];
 			}
 		}
 	}
-	//
 	int najveci = nur[0];
-	for (int i = 1; i < r; i++) {
+	for (int i=1; i < r; ++i) {
 		if (nur[i] > najveci) {
 			najveci = nur[i];
 		}
 	}
-	cout << "Trazeni broj je: " << najveci << "." << endl;
-	//
-	for (int i = 0; i < r; i++) {
-		for (int j = 0; j < k; j++) {
+	cout << "Trazeni broj je " << najveci << ", nalazi se" << endl;
+	for (int i=0; i < r; ++i) {
+		for (int j=0; j < k; ++j) {
 			if (matrica[i][j] == najveci) {
-				cout << "Nalazi se u " << (i+1) << ". redu i " << (j+1) << ". koloni. " << endl;
+				cout << "u " << (i+1) << ". redu i " << (j+1) << ". koloni" << endl;
 			}
 		}
 	}	
