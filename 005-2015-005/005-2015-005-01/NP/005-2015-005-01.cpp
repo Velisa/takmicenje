eeffac180ea88cf01e3d29cc2758489dd3b461b5
Novak Petrovic <npetrovic@gmail.com>
2017-11-19 19:10:09 +0000

    Solved 005-2015-005-01

---
+++ 005-2015-005/005-2015-005-01/NP/005-2015-005-01.cpp
@@ -0,0 +1,35 @@
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+using namespace std;
+
+int main (void) {
+  int N;
+  cin >> N;
+  int A[N];
+  float B[2*N];
+  int suma = 0;
+  for (int i=0; i < N; ++i) {
+    cin >> A[i];
+		suma += A[i];
+	}
+	for (int i=0; i < N; i++) {
+		B[2*i] = A[i];
+	}
+	B[1] = (float) suma / N;
+	for (int i=3; i < 2*N; i+=2) {
+    B[i] = B[i-2] + 1;
+	}
+	for (int i=0; i < 2*N; i++) {
+		if (i % 2 == 0) {
+      // Ovdje moramo koristiti defaultfloat
+      // da ne dobijamo decimalni ispis za cijele brojeve
+			cout << defaultfloat << B[i] << " ";
+    }
+		else {
+      B[i] = trunc(round(B[i]*100)) / 100;
+      cout << fixed << setprecision(2) << B[i] << " ";
+    }
+	}
+  return 0;
+}
---
+++ 005-2015-005/005-2015-005-01/NP/005-2015-005-01.tests.report.txt
@@ -0,0 +1,8 @@
+Testing 01
+........................TEST 01: PASS
+Testing 02
+........................TEST 02: PASS
+Testing 03
+........................TEST 03: PASS
+Testing 04
+........................TEST 04: PASS
