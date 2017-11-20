7f607ffd0a1607e6499b4a74b1b9f0b0e00447d4
Novak Petrovic <npetrovic@gmail.com>
2017-11-20 12:49:26 +0000

    Modified 005-2014-005-03

--- 005-2014-005/005-2014-005-03/NP/005-2014-005-03.cpp
+++ 005-2014-005/005-2014-005-03/NP/005-2014-005-03.cpp
@@ -1,40 +1,41 @@
-/* Copyright (c) Novak Petrovic GNU General Public License v3.0 */
+/* Copyright (c) Novak Petrovic
+ * Released under GNU General Public License v3.0 */
 
 #include <iostream>
-
 using namespace std;
 
-int main (void) { 
-	int N;
-	int indeks_prosti = 0;
-	int indeks_slozeni = 0;
-	cin >> N;
-	int niz[N], prosti_niz[N], slozeni_niz[N];
-	for (int i = 0; i < N; i++) {
-		cin >> niz[i];
-	}
-	bool prosti;
-	for (int i = 0; i < N; i++) {
-		prosti = true;
-		for (int j = 2; j < niz[i]; j++) {
-			if (niz[i] % j == 0) {
-				prosti = false;
-			}
-		}
-		if (prosti) {
-			prosti_niz[indeks_prosti] = niz[i];
-			indeks_prosti++;
-		}
-		else {
-			slozeni_niz[indeks_slozeni] = niz[i];
-			indeks_slozeni++;
-		}
-	}
-	for (int i = 0; i < indeks_prosti; i++) {
-		cout << prosti_niz[i] << endl;
-	}
-	for (int i = 0; i < indeks_slozeni; i++) {
-		cout << slozeni_niz[i] << endl;
-	}
-  return 0;
+int main(void) {
+  int N;
+  cin >> N;
+  int niz[N];
+  int prostiNiz[N];
+  int slozeniNiz[N];
+  int indeksProsti = 0;
+  int indeksSlozeni = 0;
+  for (int i = 0; i < N; ++i) {
+    cin >> niz[i];
+  }
+  bool prosti = true;
+  for (int i = 0; i < N; ++i) {
+    prosti = true;
+    for (int j = 2; j < niz[i]; ++j) {
+      if (niz[i] % j == 0) {
+        prosti = false;
+      }
+    }
+    if (prosti) {
+      prostiNiz[indeksProsti] = niz[i];
+      ++indeksProsti;
+    } else {
+      slozeniNiz[indeksSlozeni] = niz[i];
+      ++indeksSlozeni;
+    }
+  }
+  for (int i = 0; i < indeksProsti; ++i) {
+    cout << prostiNiz[i] << endl;
+  }
+  for (int i = 0; i < indeksSlozeni; ++i) {
+    cout << slozeniNiz[i] << endl;
+  }
+  return EXIT_SUCCESS;
 }
