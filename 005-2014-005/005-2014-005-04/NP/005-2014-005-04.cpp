4622d1b952e707f1e854d84d8467a9d03fecca8c
Novak Petrovic <npetrovic@gmail.com>
2017-11-20 16:20:19 +0000

    Solved 005-2014-005-04

--- 005-2014-005/005-2014-005-04/NP/005-2014-005-04.cpp
+++ 005-2014-005/005-2014-005-04/NP/005-2014-005-04.cpp
@@ -1,33 +1,35 @@
-/* Copyright (c) Novak Petrovic GNU General Public License v3.0 */
+/* Copyright (c) Novak Petrovic
+ * Released under GNU General Public License v3.0 */
 
 #include <iostream>
-
 using namespace std;
 
-int main (void) { 
-	int N, MAX;
-	cin >> N;
-	int A[N], B[N];
-	for (int i = 0; i < N; i++) {
-		cin >> A[i];
-	}
-	for (int i = 0; i < N; i++) {
-		MAX = A[0];
-		for (int j = 0; j <= i; j++) {
-			if (A[j] >= MAX) {
-				MAX = A[j];
-				B[i] = MAX;
-			}
-		}
-	}
-	cout << "Pocetni niz je: " << A[0];
-	for (int i = 1; i < N; i++) {
-		cout << "," << A[i];
-	}
-	cout << endl;
-	cout <<  "Novi niz je: " << B[0];
-	for (int i = 1; i < N; i++) {
-		cout << "," << B[i];
-	}
-	return 0;
+int main(void) {
+  int N;
+  cin >> N;
+  int A[N];
+  int B[N];
+  for (int i = 0; i < N; ++i) {
+    cin >> A[i];
+  }
+  int najveci;
+  for (int i = 0; i < N; ++i) {
+    najveci = A[0];
+    for (int j = 0; j <= i; ++j) {
+      if (A[j] >= najveci) {
+        najveci = A[j];
+        B[i] = najveci;
+      }
+    }
+  }
+  cout << "Pocetni niz je: " << A[0];
+  for (int i = 1; i < N; ++i) {
+    cout << "," << A[i];
+  }
+  cout << endl;
+  cout << "Novi niz je: " << B[0];
+  for (int i = 1; i < N; ++i) {
+    cout << "," << B[i];
+  }
+  return EXIT_SUCCESS;
 }
