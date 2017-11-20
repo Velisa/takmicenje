d2bb2c331e9f0e82fa44ed3d4e6ce7d88c46aa6a
Novak Petrovic <npetrovic@gmail.com>
2017-11-20 16:43:00 +0000

    Solved 005-2014-005-05

---
+++ 005-2014-005/005-2014-005-05/NP/005-2014-005-05.cpp
@@ -0,0 +1,29 @@
+/* Copyright (c) Novak Petrovic
+ * Released under GNU General Public License v3.0 */
+
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  string tekst;
+  getline(cin, tekst);
+  for (int i = 0; i < tekst.length(); ++i) {
+    switch (tekst[i]) {
+      case 'a':
+      case 'A':
+      case 'e':
+      case 'E':
+      case 'i':
+      case 'I':
+      case 'o':
+      case 'O':
+      case 'u':
+      case 'U':
+        break;
+      default:
+        cout << tekst[i];
+        break;
+    }
+  }
+  return EXIT_SUCCESS;
+}
