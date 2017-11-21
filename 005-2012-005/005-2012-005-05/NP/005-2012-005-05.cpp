e44ce801617c6ecb906775963a0b2a383e8aa733
Novak Petrovic <npetrovic@gmail.com>
2017-11-21 10:08:52 +0000

    Solved 005-2012-005-05

---
+++ 005-2012-005/005-2012-005-05/NP/005-2012-005-05.cpp
@@ -0,0 +1,46 @@
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  int N;
+  int jovan = 0;
+  int marko = 0;
+  cin >> N;
+  int potezi[N];
+  for (int i = 0; i < N; ++i) {
+    cin >> potezi[i];
+  }
+  bool igraJovan = true;
+  for (int i = 0; i < N; ++i) {
+    if (igraJovan) {
+      jovan += potezi[i];
+      if (jovan == marko) {
+        marko = 0;
+      }
+      if (potezi[i] == 6) {
+        igraJovan = true;
+      } else {
+        igraJovan = false;
+      }
+    } else {
+      marko += potezi[i];
+      if (marko == jovan) {
+        jovan = 0;
+      }
+      if (potezi[i] == 6) {
+        igraJovan = false;
+      } else {
+        igraJovan = true;
+      }
+    }
+  }
+  if (igraJovan) {
+    cout << "JOVAN" << endl;
+  } else {
+    cout << "MARKO" << endl;
+  }
+  cout << jovan << endl;
+  cout << marko;
+  return EXIT_SUCCESS;
+}
