79152542d6452fd19e383912ba462ab18ea92610
Novak Petrovic <npetrovic@gmail.com>
2017-11-21 10:11:17 +0000

    Solved 005-2012-005-01

---
+++ 005-2012-005/005-2012-005-01/NP/005-2012-005-01.cpp
@@ -0,0 +1,31 @@
+#include <math.h>
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  int tacke[3][2];
+  //     Ax             Ay
+  cin >> tacke[0][0] >> tacke[0][1];
+  //     Bx             By
+  cin >> tacke[1][0] >> tacke[1][1];
+  //     Cx             Cy
+  cin >> tacke[2][0] >> tacke[2][1];
+  int a, b, c, o, p, s;
+  //           Ax            Bx
+  a = sqrt(pow(tacke[0][0] - tacke[1][0], 2) +
+           //           Ay            By
+           pow(tacke[0][1] - tacke[1][1], 2));
+  //           Bx            Cx
+  b = sqrt(pow(tacke[1][0] - tacke[2][0], 2) +
+           //           By            Cy
+           pow(tacke[1][1] - tacke[2][1], 2));
+  //           Cx            Ax
+  c = sqrt(pow(tacke[2][0] - tacke[0][0], 2) +
+           //           Cy            Ay
+           pow(tacke[2][1] - tacke[0][1], 2));
+  o = a + b + c;
+  s = o / 2;
+  cout << "O = " << o << endl;
+  cout << "P = " << sqrt(s * (s - a) * (s - b) * (s - c));
+  return EXIT_SUCCESS;
+}
