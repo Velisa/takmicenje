76d6bac56c55d70d39f419437024c4714cfc58ff
Novak Petrovic <npetrovic@gmail.com>
2017-11-21 10:06:46 +0000

    Solved 005-2012-005-02

---
+++ 005-2012-005/005-2012-005-02/NP/005-2012-005-02.cpp
@@ -0,0 +1,31 @@
+#include <math.h>
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  int N, M;
+  cin >> N;
+  cin >> M;
+  int A[N][M];
+  for (int i = 0; i < N; ++i) {
+    for (int j = 0; j < M; ++j) {
+      cin >> A[i][j];
+    }
+  }
+  int zbir = 0;
+  for (int i = 0; i < N; ++i) {
+    zbir = 0;
+    for (int j = 0; j < M; ++j) {
+      zbir += A[i][j];
+    }
+    cout << "Zbir el. " << i + 1 << ". vrste je " << zbir << endl;
+  }
+  for (int i = 0; i < M; ++i) {
+    int zbir = 0;
+    for (int j = 0; j < N; ++j) {
+      zbir += A[j][i];
+    }
+    cout << "Zbir el. " << i + 1 << ". kolone je " << zbir << endl;
+  }
+  return EXIT_SUCCESS;
+}
