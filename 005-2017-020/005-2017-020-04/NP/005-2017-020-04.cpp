b94cd8c7e3bccacb880f9c11a995f54c9891e9cb
Novak Petrovic <npetrovic@gmail.com>
2017-11-29 16:21:17 +0000

    Verzija koja prolazi 8 od 10 testova

--- 005-2017-020/005-2017-020-04/NP/005-2017-020-04.cpp
+++ 005-2017-020/005-2017-020-04/NP/005-2017-020-04.cpp
@@ -16,57 +16,67 @@ int main() {
       }
     }
   }
-
-  // Potrebo je uspostaviti petlju za K, ovo je samo za jedan pokusaj
-
-  // Prebrojati nule u redovima
-  int nule_redovi[N];
-  for (int i = 0; i < N; ++i) nule_redovi[i] = 0;
-  for (int i = 0; i < N; ++i) {
-    for (int j = 0; j < M; ++j) {
-      if (niz[i][j] == 0) {
-        ++nule_redovi[i];
-      }
-    }
+  if (N == 1 && M == 1 && niz[0][0] == 1) {
+    cout << 1;
+    return 0;
   }
-  // Prebrojati nule u kolonama sa kvalifikovanim redovima
-  int nule_kolone[M];
-  for (int j = 0; j < M; ++j) {
-    for (int i = 0; i < N; ++i) {
-      if (niz[i][j] == 0 && nule_redovi[i] <= K) {
-        ++nule_kolone[j];
-      }
-    }
-  }
-  // Pronaci kolonu sa najvise nula u kvalifikovanim redovima
-  int kol_max = 0;
-  int indeks_kol_maks;
-  for (int i = 0; i < M; ++i) {
-    if (nule_kolone[i] > kol_max) {
-      kol_max = nule_kolone[i];
-      indeks_kol_maks = i;
-    }
-  }
-  // Jedna "akcija" nad kolonom
-  for (int i = 0; i < N; ++i) {
-    if (niz[i][indeks_kol_maks] == 0) {
-      niz[i][indeks_kol_maks] = 1;
-    } else {
-      niz[i][indeks_kol_maks] = 0;
-    }
-  }
-  // Prebrojavanje upaljenih redova
-  int redova = 0;
   bool red_upaljen;
-  for (int i = 0; i < N; ++i) {
-    red_upaljen = true;
-    for (int j = 0; j < M; ++j) {
-      if (niz[i][j] == 0) {
-        red_upaljen = false;
+  int redova;
+  int kol_maks;
+  int nule_redovi[N];
+  int nule_kolone[M];
+  int indeks_kol_maks;
+  // Prebrojati nule u redovima
+  for (int kk = 0; kk < K; ++kk) {
+    for (int i = 0; i < N; ++i) {
+      nule_redovi[i] = 0;
+    }
+    for (int i = 0; i < N; ++i) {
+      for (int j = 0; j < M; ++j) {
+        if (niz[i][j] == 0) {
+          ++nule_redovi[i];
+        }
       }
     }
-    if (red_upaljen) {
-      ++redova;
+    // Prebrojati nule u kolonama sa kvalifikovanim redovima
+    for (int i = 0; i < M; ++i) {
+      nule_kolone[i] = 0;
+    }
+    for (int j = 0; j < M; ++j) {
+      for (int i = 0; i < N; ++i) {
+        if (niz[i][j] == 0 && nule_redovi[i] <= K) {
+          ++nule_kolone[j];
+        }
+      }
+    }
+    // Pronaci kolonu sa najvise nula u kvalifikovanim redovima
+    kol_maks = 0;
+    for (int i = 0; i < M; ++i) {
+      if (nule_kolone[i] > kol_maks) {
+        kol_maks = nule_kolone[i];
+        indeks_kol_maks = i;
+      }
+    }
+    // Jedna "akcija" nad kolonom
+    for (int i = 0; i < N; ++i) {
+      if (niz[i][indeks_kol_maks] == 0) {
+        niz[i][indeks_kol_maks] = 1;
+      } else {
+        niz[i][indeks_kol_maks] = 0;
+      }
+    }
+    // Prebrojavanje upaljenih redova
+    redova = 0;
+    for (int i = 0; i < N; ++i) {
+      red_upaljen = true;
+      for (int j = 0; j < M; ++j) {
+        if (niz[i][j] == 0) {
+          red_upaljen = false;
+        }
+      }
+      if (red_upaljen) {
+        ++redova;
+      }
     }
   }
   // Ispis redova
--- 005-2017-020/005-2017-020-04/NP/005-2017-020-04.tests.report.tst
+++ 005-2017-020/005-2017-020-04/NP/005-2017-020-04.tests.report.tst
@@ -2,3 +2,19 @@ Testing 01
 ........................TEST 01: PASS
 Testing 02
 ........................TEST 02: PASS
+Testing 03
+........................TEST 03: PASS
+Testing 04
+........................TEST 04: PASS
+Testing 05
+........................TEST 05: PASS
+Testing 06
+........................TEST 06: PASS
+Testing 07
+........................TEST 07: FAIL
+Testing 08
+........................TEST 08: PASS
+Testing 09
+........................TEST 09: PASS
+Testing 10
+........................TEST 10: FAIL
