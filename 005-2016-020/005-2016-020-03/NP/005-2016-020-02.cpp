f972529be067d2d8ddee68c3ac88d0359c3f65a8
Novak Petrovic <npetrovic@gmail.com>
2017-12-05 14:52:44 +0000

    Zadatak 005-2016-020-06 rijesen

---
+++ 005-2016-020/005-2016-020-03/NP/005-2016-020-02.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+	int x[N], y[N];
+	for(int i = 0; i < N; ++i) cin >> x[i] >> y[i];
+	int M;
+	cin >> M;
+	int x1[M], x2[M], y1[M], y2[M];
+	for(int i = 0; i < M; ++i) cin >> x1[i] >> y1[i] >> x2[i] >> y2[i];
+	//
+	int borova = 0;
+	for (int i = 0; i < M; ++i) {
+		for (int j = 0; j < N; ++j) {
+			if	(
+					// Borovi moraju biti u okviru setalista
+					(x[j] >= x1[i] && x[j] <= x2[i] && y[j] >= y1[i] && y[j] <= y2[i])
+					&& 
+					// Borovi moraju biti na ivicama setalista
+					(x[j] == x1[i] || x[j] == x2[i] || y[j] == y1[i] || y[j] == y2[i])
+				) {
+					++borova;
+			}
+		}
+		cout << borova << endl;
+		borova = 0;
+	}
+	return 0;
+}
